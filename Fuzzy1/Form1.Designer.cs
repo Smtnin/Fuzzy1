7f5d618950d95ce82b628f31e4d04df3869ad416
Smtnin <smithxbc@yandex.ru>
2012-08-29 00:50:43 +0400

    one

---
+++ Fuzzy1/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(47, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(416, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Принятие решений в нечеткой среде";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 1;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Location = new System.Drawing.Point(31, 96);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(291, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Минимально значащая оценка по точным дисциплинам";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(161, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(294, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Минимально значащая оценка по иностранным языкам";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 1;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown2.Location = new System.Drawing.Point(31, 122);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(161, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(297, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Максимально значащая оценка по точным дисциплинам";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.DecimalPlaces = 1;
+            this.numericUpDown3.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown3.Location = new System.Drawing.Point(31, 148);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(158, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(300, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Максимально значащая оценка по иностранным языкам";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.DecimalPlaces = 1;
+            this.numericUpDown4.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown4.Location = new System.Drawing.Point(31, 174);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown4.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(47, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(245, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Наилучшее решение:";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(52, 277);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 24);
+            this.listView1.TabIndex = 11;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(469, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(273, 228);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(221, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 32);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "I\'m feeling lucky";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(31, 322);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(782, 244);
+            this.dataGridView2.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1157, 607);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ Fuzzy1/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //это быдло-код по книге "Методы и технологии искусственного интелелекта" Л.Рутковский
+            // стр. 162 Пример 4.52
+            // Йиихаа
+
+            InitializeComponent();            
+            InitializeDataGridView();
+            numericUpDown1.Value = 4.3m;
+            numericUpDown2.Value = 4.8m;
+            numericUpDown3.Value = 4.2m;
+            numericUpDown4.Value = 4.6m;
+            InitGRAPH();
+
+            StreamReader f = new StreamReader("test.txt");
+            string s;
+            const int n = 20;
+            double[] a = new double[n];
+            string[] buf;
+            while ((s = f.ReadLine()) != null)
+            {
+                buf = s.Split(' ');
+                double sum = 0;
+                for (int i = 0; i < buf.Length; ++i)
+                {
+                    a[i] = Convert.ToDouble(buf[i]);
+                    sum += a[i];
+                }
+
+
+
+            }
+            double a1 = Convert.ToDouble(numericUpDown1.Value);
+        }
+
+        private void InitGRAPH()
+        {
+            int wX;
+            int hX;
+            double xF, yF;
+            double step;
+
+            wX = pictureBox1.Width;
+            hX = pictureBox1.Height; //Значение высоты
+
+            // Система Координат
+            Bitmap flag = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics flagGraphics = Graphics.FromImage(flag);
+            System.Drawing.Pen myPen;
+            myPen = new System.Drawing.Pen(System.Drawing.Color.Blue);
+            flagGraphics.DrawLine(myPen, 25, hX-15, wX, hX-15);
+            flagGraphics.DrawLine(myPen, 30, 0, 30, hX-12);
+            Font dew = new Font("Verdana", 8, FontStyle.Bold);
+            System.Drawing.Brush Brush = new SolidBrush(Color.Gray);
+            flagGraphics.DrawString("    4,0    4,2    4,4    4,6    4,8    5,0         x", dew, Brush, 3, hX-12);
+
+            flagGraphics.DrawString("0", dew, Brush, 6, hX - 22);
+            flagGraphics.DrawString("0,2", dew, Brush, 0, hX - 45);
+            flagGraphics.DrawString("0,4", dew, Brush, 0, hX - 75);
+            flagGraphics.DrawString("0,6", dew, Brush, 0, hX - 105);
+            flagGraphics.DrawString("0,8", dew, Brush, 0, hX - 135);
+            flagGraphics.DrawString("1,0", dew, Brush, 0, hX - 165);
+            flagGraphics.DrawString("u", dew, Brush, 6, hX - 195);
+            
+            // График
+            myPen = new System.Drawing.Pen(System.Drawing.Color.Red);
+            flagGraphics.DrawLine(myPen, 25, hX - 15, wX, hX - 15);
+            flagGraphics.DrawLine(myPen, 30, 0, 30, hX - 12);
+
+
+            pictureBox1.Image = flag;
+
+
+        }
+
+        private void InitializeDataGridView()
+        {
+            // Create an unbound DataGridView by declaring a column count.
+            dataGridView2.ColumnCount = 6;
+            dataGridView2.ColumnHeadersVisible = true;
+
+            // Set the column header style.
+            DataGridViewCellStyle columnHeaderStyle = new DataGridViewCellStyle();
+
+            columnHeaderStyle.BackColor = Color.Beige;
+            columnHeaderStyle.Font = new Font("Verdana", 8, FontStyle.Bold);
+            dataGridView2.ColumnHeadersDefaultCellStyle = columnHeaderStyle;
+
+            // Set the column header names.
+            dataGridView2.Columns[0].Name = "Студент";
+            dataGridView2.Columns[1].Name = "Электроника";
+            dataGridView2.Columns[2].Name = "Информатика";
+            dataGridView2.Columns[3].Name = "Математика";
+            dataGridView2.Columns[4].Name = "Английский";
+            dataGridView2.Columns[5].Name = "Немецкий";
+
+            // Populate the rows.
+            string[] row1 = new string[] { "Катя(х1)", "4,8", "5,0", "4,7", "4,3", "4,7" };
+            string[] row2 = new string[] { "Маша(х2)", "4,4", "4,7", "4,8", "4,4", "4,4" };
+            string[] row3 = new string[] { "Аня(х3)", "4,9", "4,9", "4,6", "4,7", "4,3" };
+            string[] row4 = new string[] { "Томек(х4)", "4,5", "4,8", "4,9", "5,0", "4,5" };
+            string[] row5 = new string[] { "Яцек(х5)", "5,0", "4,6", "4,7", "4,4", "5,0" };
+            string[] row6 = new string[] { "Михаил(х6)", "4,9", "4,5", "5,0", "4,5", "4,4" };
+            object[] rows = new object[] { row1, row2, row3, row4, row5, row6 };
+
+            foreach (string[] rowArray in rows)
+            {
+                dataGridView2.Rows.Add(rowArray);
+            }
+        }
+    }
+       
+}
